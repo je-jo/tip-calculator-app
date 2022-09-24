db9f376b4ab477809d8ef9f74276cd506c439be9
Jelena Jovicic <jejovicic@gmail.com>
2022-09-24 21:43:07 +0200

    Some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 let bill = 0;
-let percent = 0.15;
-let numberOfPersons = 0;
-const displayTip = document.getElementById("tip");
-const displayTotalBill = document.getElementById("total-bill");
-const buttonClear = document.getElementById("clear");
+let percent = 0;
+let numberOfPersons = 1;
+
+//main functions
 
 let calculateIndividualBill = () => bill / numberOfPersons;
 let calculateIndividualTip = () => calculateIndividualBill() * percent;
 let calculateTotalBillPerPerson = () => calculateIndividualBill() + calculateIndividualTip();
 
+//get values from input
+
 const inputBill = document.getElementById("bill");
 inputBill.addEventListener("input", updateBillValue);
-
 function updateBillValue(e) {
     bill = e.target.value;
-    calculateIndividualBill();
+    updateDisplay();
 }
 
 const inputPercent = [...document.querySelectorAll('input[name="percentage"]')];
@@ -25,19 +25,18 @@ for (let i = 0; i < inputPercent.length; i++) {
         } else {
             percent = e.currentTarget.value;
         }
-        console.log(percent)
         updateDisplay();
     });
 }
 
 const inputPeople = document.getElementById("people");
 inputPeople.addEventListener("input", updateNumberOfPersons);
-
 function updateNumberOfPersons(e) {
     numberOfPersons = e.target.value;
-    calculateIndividualBill();
     updateDisplay();
 }
+
+//helper functions
 
 function formatPrice(num) {
     return num.toLocaleString("en-US", {
@@ -48,16 +47,20 @@ function formatPrice(num) {
     });
 }
 
+const displayTip = document.getElementById("tip");
+const displayTotalBill = document.getElementById("total-bill");
 function updateDisplay() {
     displayTip.textContent = formatPrice(calculateIndividualTip());
     displayTotalBill.textContent = formatPrice(calculateTotalBillPerPerson());
 }
 
+const buttonClear = document.getElementById("clear");
+const form = document.querySelector("form");
 function clearAll() {
     bill = 0;
     percent = 0;
-    numberOfPersons = 0;
+    numberOfPersons = 1;
+    form.reset();
     updateDisplay();
 }
-
 buttonClear.addEventListener("click", clearAll);
